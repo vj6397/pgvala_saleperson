68904d1054765a66c57b740d53e8baf6edbfdc1a
vj6397 <vjjaat06122001@gmail.com>
2023-08-06 02:04:39 +0530

    Initial commit

---
+++ lib/Api/request_util.dart
@@ -0,0 +1,46 @@
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RequestUtil{
+  final endpoint='https://davaivala.shop/';
+  final token='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MjIxNjM4NDAsImlhdCI6MTY5MDYyNzg0MCwic2NvcGUiOiJhY2Nlc3NfdG9rZW4iLCJzdWIiOiJhY2MwNiJ9.D9wDOUSPuy0RX_QOH0Zhx8QiSZ1GLo3jfoaHvRj39tE';
+
+  Future<http.Response> resendOtp (number){
+    return http.post(Uri.parse(endpoint+'resend_otp?contact=$number&user_type=landlord'));
+  }
+
+  Future<http.Response> login(number, otp){
+    return http.post(Uri.parse(endpoint+'app_login'),
+        headers: {"accept": "application/json",
+          "Content-Type": "application/json"},
+        body: jsonEncode ({"contact": number, "otp": otp,"user_type": "landlord"}));
+  }
+
+  Future<http.Response> roomList() async{
+    return  http.get(Uri.parse(endpoint+'room_list/'),
+      headers: {'Authorization': token},
+    );
+  }
+  Future<http.Response> cityList() async{
+    return  http.get(Uri.parse(endpoint+'get_city_list/'),
+      headers: {'Authorization': token},
+    );
+  }
+  // Future<http.Response> ownerDetail()  async{
+  //   return http.get(Uri.parse(endpoint+'accommodation_detail/'),
+  //     headers: {'Authorization': token},
+  //   );
+  // }
+  // Future<http.Response> update(roomid,field,value) async{
+  //   return  http.put(Uri.parse(endpoint+'room_detail_update/?roomid=$roomid&field=$field&value=$value'),
+  //     headers: {'Authorization': token},
+  //   );
+  // }
+  Future<http.Response> sales_logIn(sales_id,password)async{
+    return http.put(Uri.parse(endpoint+'sales_login/?sales_id=$sales_id&password=$password'),
+        headers: {'Authorization': token},
+    );
+  }
+
+}
---
+++ lib/utils/location_list.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+// import 'package:pgvala_saleperson/Api/request_util.dart';
+
+final keyVal='Login';
+List states=['madya pradesh','rajisthan','chattisgarh'];
+List cities=['Bhilai','Raipur'];
+List localities=['hello','how','are'];
+List rooomsharing=['Single Sharing','Double Sharing','Three Sharing','1bhk','2bhk','3bhk'];
+List rate =['Day','Hour','Monthly','Yearly'];
